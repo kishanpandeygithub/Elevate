7ba38a8256688c1a8d96a6544877d278f93ceaab
kishan pandey <145040652+kishanpandeygithub@users.noreply.github.com>
2026-06-17 23:29:09 +0530

    Change axios base URL to production endpoint

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL:"http://localhost:4000",
+    baseURL:"https://elevate-backend-6lqz.onrender.com",
     withCredentials:true ,
     headers:{
         'Content-Type':'application/json'
